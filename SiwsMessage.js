9a83b9bde8807819a1165fde37a1c558fd3eaf7e
pbtripletree <paul@tripletreellc.com>
2022-02-14 15:03:15 -0700

    prepareMessage fix

--- SiwsMessage.js
+++ SiwsMessage.js
@@ -10,7 +10,7 @@ class SiwsMessage {
   prepareMessage() {
     const nonce = crypto.randomBytes(16).toString("base64");
     const timestamp = new Date();
-    const message = `${this.domain} wants you to sign in with your Solana account:\n${this.address}\n\n${this.statement}.\n\nNonce: ${this.nonce}\nIssued At: ${this.timestamp}`;
+    const message = `${this.domain} wants you to sign in with your Solana account:\n${this.address}\n\n${this.statement}.\n\nNonce: ${nonce}\nIssued At: ${timestamp}`;
     return new TextEncoder().encode(message);
   }
 }
